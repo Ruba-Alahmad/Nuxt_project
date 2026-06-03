30e97b1a5143757a4039e6e06dfb12237308a7fc
Ruba-Alahmad <rubaalahmad393@gmail.com>
2026-06-03 15:37:19 +0300

    Setup the project

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet, 
+            consectetur adipisicing elit. 
+            Laborum sed soluta nemo dolorum consequatur deserunt fugit veniam, 
+        </p>
+        <p>Lorem ipsum dolor sit amet, 
+            consectetur adipisicing elit. 
+            Laborum sed soluta nemo dolorum consequatur deserunt fugit veniam, 
+        </p>
+    </div>
+</template>
+
+<script setup>
+
+ </script>
+
+<style  scoped>
+    h2{
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p{
+        margin: 20px 0;
+    }
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet, 
+            consectetur adipisicing elit. 
+            Laborum sed soluta nemo dolorum consequatur deserunt fugit veniam, 
+        </p>
+    </div>
+</template>
+
+<script setup>
+const { id} = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
